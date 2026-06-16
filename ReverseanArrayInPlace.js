487cf9deb780ef0a35bf228a0af2865c03252879
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-06-16 21:49:54 +0530

    Create ReverseanArrayInPlace.js

---
+++ ReverseanArrayInPlace.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 4, 5];
+
+let left = 0;
+let right = arr.length - 1;
+
+while (left < right) {
+  [arr[left], arr[right]] = [arr[right], arr[left]];
+  left++;
+  right--;
+}
+
+console.log(arr);
