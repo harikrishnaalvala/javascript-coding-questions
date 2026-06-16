20fa46623cb5d076096462bf87cf5805bb214411
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-06-16 21:50:37 +0530

    Add function to find largest and smallest elements

---
+++ FindtheLargestandSmallestElement.js
@@ -0,0 +1,12 @@
+let arr = [10, 5, 20, 8, 3];
+
+let largest = arr[0];
+let smallest = arr[0];
+
+for (let num of arr) {
+  if (num > largest) largest = num;
+  if (num < smallest) smallest = num;
+}
+
+console.log("Largest:", largest);
+console.log("Smallest:", smallest);
