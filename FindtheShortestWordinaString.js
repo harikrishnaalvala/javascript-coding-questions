94fc88a31b495ff0a762c58bb69662720fe56123
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-06-18 23:17:21 +0530

    Update FindtheShortestWordinaString.js

--- FindtheShortestWordinaString.js
+++ FindtheShortestWordinaString.js
@@ -4,3 +4,17 @@ def shortest_word(s):
 
 text = "Python is a powerful programming language"
 print(shortest_word(text)) // a
+
+// Without min() :
+
+def shortest_word(s):
+    words = s.split()
+    shortest = words[0]
+
+    for word in words:
+        if len(word) < len(shortest):
+            shortest = word
+
+    return shortest
+
+print(shortest_word("Python is a powerful programming language")) //a
