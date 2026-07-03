c7af6eb18941166835c15d315310882ee3e3defd
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-03 16:02:27 +0530

    Create countVowels.js

---
+++ countVowels.js
@@ -0,0 +1,14 @@
+function countVowels(str) {
+    let count = 0;
+    const vowels = "aeiouAEIOU";
+
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log(countVowels("Hello World")); // 3
