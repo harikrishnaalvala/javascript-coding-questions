e4015f63d86a5053e9b6558d5e8efd94cb89546e
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-03 16:01:12 +0530

    Update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -3,3 +3,17 @@ function reverseString(str) {
 }
 
 console.log(reverseString("hello")); // olleh
+
+// Method 2: Using a Loop :
+
+function reverseString(str) {
+    let reversed = '';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+
+    return reversed;
+}
+
+console.log(reverseString("hello")); // olleh
