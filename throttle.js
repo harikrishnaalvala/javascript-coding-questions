f5352d295f2273f346e089530e7d095c966dcca4
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-04 18:01:20 +0530

    Create throttle.js

---
+++ throttle.js
@@ -0,0 +1,21 @@
+function throttle(func, delay) {
+    let lastCall = 0;
+
+    return function (...args) {
+        const now = Date.now();
+
+        if (now - lastCall >= delay) {
+            lastCall = now;
+            func(...args);
+        }
+    };
+}
+function logMessage() {
+    console.log("Executed:", Date.now());
+}
+
+const throttledLog = throttle(logMessage, 2000);
+
+setInterval(() => {
+    throttledLog();
+}, 500);
