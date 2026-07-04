9da70ffac2b3a9a0c21f0434b65ab168f44e2a0a
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-04 17:57:32 +0530

    Create objectToQueryString.js

---
+++ objectToQueryString.js
@@ -0,0 +1,15 @@
+function objectToQueryString(obj) {
+    return Object.entries(obj)
+        .map(([key, value]) =>
+            `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+        )
+        .join('&');
+}
+
+const params = {
+    name: "John",
+    age: 25,
+    city: "New York"
+};
+
+console.log(objectToQueryString(params));
