6a61e58d6c9ff9a466753575af240eeb364f426d
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-05 16:30:47 +0530

    Implement containsValue function with multiple methods
    Added multiple methods to check if an array contains a value.

--- containsValue.js
+++ containsValue.js
@@ -4,3 +4,25 @@ function containsValue(arr, value) {
 
 console.log(containsValue([1, 2, 3], 2)); // true
 console.log(containsValue([1, 2, 3], 4)); // false
+
+// Method 2: Using indexOf() :
+
+function containsValue(arr, value) {
+    return arr.indexOf(value) !== -1;
+}
+
+console.log(containsValue([1, 2, 3], 2)); // true
+console.log(containsValue([1, 2, 3], 4)); // false
+
+// Method 3: Without Built-in Methods :
+function containsValue(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(containsValue([1, 2, 3], 2)); // true
+console.log(containsValue([1, 2, 3], 4)); // false
