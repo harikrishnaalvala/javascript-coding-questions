ce88a06367bda73ca2a2e3fc1432aef4ac612899
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-06 22:44:43 +0530

    Update repeatString.js

--- repeatString.js
+++ repeatString.js
@@ -1,5 +1,29 @@
+// Method 1: Using the built-in repeat() method :
+
 function repeatString(str, n) {
     return str.repeat(n);
+}
+
+console.log(repeatString("abc", 3)); // "abcabcabc"
+
+// Method 2: Using a loop :
+
+function repeatString(str, n) {
+    let result = "";
+
+    for (let i = 0; i < n; i++) {
+        result += str;
+    }
+
+    return result;
+}
+
+console.log(repeatString("abc", 3)); // "abcabcabc"
+
+// Method 3: Using Array.join() :
+
+function repeatString(str, n) {
+    return new Array(n + 1).join(str);
 }
 
 console.log(repeatString("abc", 3)); // "abcabcabc"
