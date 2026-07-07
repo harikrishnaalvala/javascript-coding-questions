4cfd97188788c5ad8146bda22399acaf3b92518f
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-07 14:57:22 +0530

    Update getSubstrings.js

--- getSubstrings.js
+++ getSubstrings.js
@@ -12,4 +12,20 @@ function getSubstrings(str) {
     return result;
 }
 
-console.log(getSubstrings("abc"));
+console.log(getSubstrings("abc")); // ["a", "ab", "abc", "b", "bc", "c"]
+
+// Method 2: Using slice() :
+
+function getSubstrings(str) {
+    const result = [];
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i + 1; j <= str.length; j++) {
+            result.push(str.slice(i, j));
+        }
+    }
+
+    return result;
+}
+
+console.log(getSubstrings("dog")); //  ["d", "do", "dog", "o", "og", "g"]
