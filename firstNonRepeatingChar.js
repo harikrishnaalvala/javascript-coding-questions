45f02c94da556450618e0e8cec5d1e1ee391eb0d
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-18 22:51:43 +0530

    Create firstNonRepeatingChar.js

---
+++ firstNonRepeatingChar.js
@@ -0,0 +1,16 @@
+function firstNonRepeatingChar(string){
+    let fre={}
+    for (let i of string){
+        fre[i]=(fre[i] || 0)+1
+        
+    }
+    for(let i of string){
+        if (fre[i]===1){
+            return i
+        }
+    }
+    return "there is no non repeating characters"
+}
+const string="amazon"
+const result=firstNonRepeatingChar(string)
+console.log(result)
