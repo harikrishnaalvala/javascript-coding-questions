7b70bbdb9f8f9a6c4d88023f47e23addbc7b130e
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-18 23:01:06 +0530

    Create charWithFrequency.js

---
+++ charWithFrequency.js
@@ -0,0 +1,22 @@
+function charWithFrequency(string){
+    if(!string){
+        return -1
+    }
+    let result=""
+    let a=string[0]
+    let count=1 
+    for(let i=1;i<string.length;i++){
+        if(string[i]===a){
+            count++
+        }else{
+            result+=a+count 
+            a=string[i]
+            count=1
+        }
+    }
+    result+=a+count 
+    return result
+}
+const string="aaabbaccd"
+const result=charWithFrequency(string)
+console.log(result)
