a463fef30ba06e70a61b465ca7e9c21416b33346
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-21 17:34:02 +0530

    Create reverseAlternateWords.js

---
+++ reverseAlternateWords.js
@@ -0,0 +1,13 @@
+function reverseAlternateWords(str) {
+    const words = str.split(".");
+
+    for (let i = 1; i < words.length; i += 2) {
+        words[i] = words[i].split("").reverse().join("");
+    }
+
+    return words.join(".");
+}
+
+// Example
+const input = "i.like.this.programme.very.much";
+console.log(reverseAlternateWords(input));
