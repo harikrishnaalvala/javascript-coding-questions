ae0178e10bcb0b26f9996e0ed2c272e6c0b86276
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-21 17:28:37 +0530

    Create countVowelsAndConsonants.js

---
+++ countVowelsAndConsonants.js
@@ -0,0 +1,22 @@
+function countVowelsAndConsonants(str) {
+    let vowels = 0;
+    let consonants = 0;
+
+    str = str.toLowerCase();
+
+    for (let ch of str) {
+        if (ch >= 'a' && ch <= 'z') {
+            if ("aeiou".includes(ch)) {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+    }
+
+    console.log("Vowel Count:", vowels);
+    console.log("Consonant Count:", consonants);
+}
+
+// Example
+countVowelsAndConsonants("Hello World");
