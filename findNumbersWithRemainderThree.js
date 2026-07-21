a882c28ff807663b8549894ef20e4438a285d661
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-21 17:26:23 +0530

    Create findNumbersWithRemainderThree.js

---
+++ findNumbersWithRemainderThree.js
@@ -0,0 +1,9 @@
+function findNumbersWithRemainderThree(nums) {
+    return nums.filter(num => num % 7 === 3);
+}
+
+// Test
+const inputArray = [7, 17, 5, 45];
+const output = findNumbersWithRemainderThree(inputArray);
+
+console.log("Output:", output);
