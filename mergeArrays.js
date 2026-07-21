df73ad4698b5b76d5e5a47bc45a70756f05a61c0
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-21 17:30:04 +0530

    Create mergeArrays.js

---
+++ mergeArrays.js
@@ -0,0 +1,23 @@
+function mergeArrays(arr1, arr2) {
+    let result = [];
+    let i = 0, j = 0;
+
+    while (i < arr1.length || j < arr2.length) {
+        if (i < arr1.length) {
+            result.push(arr1[i]);
+            i++;
+        }
+
+        if (j < arr2.length) {
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+
+    return result;
+}
+
+const arr1 = [1, 2, 3, 4];
+const arr2 = ['a', 'b'];
+
+console.log(mergeArrays(arr1, arr2));
