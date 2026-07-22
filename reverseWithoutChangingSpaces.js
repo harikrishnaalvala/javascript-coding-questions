91ba7aacfb9399845fe2c1357e1012eff1833dfd
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-22 16:21:08 +0530

    Create reverseWithoutChangingSpaces.js

---
+++ reverseWithoutChangingSpaces.js
@@ -0,0 +1,23 @@
+function reverseWithoutChangingSpaces(str) {
+    let chars = str.split("");
+    let left = 0;
+    let right = chars.length - 1;
+
+    while (left < right) {
+        if (chars[left] === " ") {
+            left++;
+        } else if (chars[right] === " ") {
+            right--;
+        } else {
+            [chars[left], chars[right]] = [chars[right], chars[left]];
+            left++;
+            right--;
+        }
+    }
+
+    return chars.join("");
+}
+
+// Example
+const input = "a bc def g";
+console.log(reverseWithoutChangingSpaces(input));
