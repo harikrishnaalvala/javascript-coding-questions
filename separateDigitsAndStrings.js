dd0d1befb030464e0ea48dab24a150ae536d8e0d
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-22 16:16:52 +0530

    Create separateDigitsAndStrings.js

---
+++ separateDigitsAndStrings.js
@@ -0,0 +1,18 @@
+function separateDigitsAndStrings(arr) {
+    let numbers = [];
+    let strings = [];
+
+    for (let item of arr) {
+        if (typeof item === "number") {
+            numbers.push(item);
+        } else if (typeof item === "string") {
+            strings.push(item);
+        }
+    }
+
+    console.log("Numbers:", numbers);
+    console.log("Strings:", strings);
+}
+
+const arr = [1, "a", 2, "b", 3, "hello", 4, "world"];
+separateDigitsAndStrings(arr);
