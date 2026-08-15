18e380b5445127e1f62ca728db00690d6de79f95
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-08-15 16:15:57 +0530

    Create twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,18 @@
+function twoSum(arr, target) {
+    let seen = new Set();
+    let pairs = [];
+
+    for (let num of arr) {
+        let complement = target - num;
+
+        if (seen.has(complement)) {
+            pairs.push([num, complement]);
+        }
+
+        seen.add(num);
+    }
+
+    return pairs;
+}
+
+console.log(twoSum([1, 2, 3, 4, 5, 6], 7));
